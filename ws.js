2699f6291e424975a59e923960a479be3a3f8d9a
klkl <kuanglong0312@sina.com>
2021-01-29 16:58:25 +0800

    fix: 数据更新和插入行导致死循环问题

--- ws.js
+++ ws.js
@@ -5,27 +5,36 @@ wss.on('connection', (ws, req) => {
   ws.on('message', msg => {
     try {
       const data = JSON.parse(msg)
-      if (data.type === 'checkalive') { // 心跳
-        ws.send(JSON.stringify({type: 'checkalive'}))
-      } else if (data.type === 'excelChange') { // 表格修改
-        cacheData = data.total
-        wss.clients.forEach(function each(client) {
-          // 通知除了当前客户端的其他客户端
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(msg)
+      switch (data.type) {
+        case 'checkalive': // 心跳
+          ws.send(JSON.stringify({type: 'checkalive'}))
+          break
+        case 'new': // 新打开页面加载历史数据
+          const newObj = {
+            type: 'history',
+            has: cacheData.length === 0 ? false : true,
+            data: cacheData
           }
-        })
-      } else if (data.type === 'new') { // 新打开页面加载历史数据
-        const newObj = {
-          type: 'history',
-          has: cacheData.length === 0 ? false : true,
-          data: cacheData
-        }
-
-        console.log(cacheData)
-        ws.send(JSON.stringify(newObj))
-      } else {
-        console.log(msg)
+          ws.send(JSON.stringify(newObj))
+          break
+        case 'excelChange': // 表格修改
+          cacheData = data.total
+          wss.clients.forEach(function each(client) {
+            // 通知除了当前客户端的其他客户端
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(msg)
+            }
+          })
+          break
+        case 'excelInsert': // 表格插入
+          cacheData = data.total
+          wss.clients.forEach(client => {
+            // 通知除了当前客户端的其他客户端
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(msg)
+            }
+          })
+          break
       }
     } catch (error) {
 
